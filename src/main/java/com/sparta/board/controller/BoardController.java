7d3ffa1e1fd22593206721ac735846bf1d967271
yunji <dbswl4342@gmail.com>
2023-07-05 10:06:48 +0900

    url 수정

--- src/main/java/com/sparta/board/controller/BoardController.java
+++ src/main/java/com/sparta/board/controller/BoardController.java
@@ -17,27 +17,27 @@ public class BoardController {
 
     private  final BoardService boardService;
 
-    @PostMapping("/board") // 게시글 작성
+    @PostMapping("/post") // 게시글 작성
     public PostResponseDto createPost(@RequestBody PostRequestDto requestDto, HttpServletRequest req){
         return boardService.createPost(requestDto, req);
     }
 
-    @GetMapping("/board") //전체 게시글 조회
+    @GetMapping("/posts") //전체 게시글 조회
     public List<PostResponseDto> getPosts(){
         return boardService.getPosts();
     }
 
-    @GetMapping("/board/{id}") // 선택 게시글 조회
+    @GetMapping("/post/{id}") // 선택 게시글 조회
     public PostResponseDto getPost(@PathVariable Long id){
         return boardService.getPost(id);
     }
 
-    @PutMapping("/board/{id}") // 게시글 수정
+    @PutMapping("/post/{id}") // 게시글 수정
     public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto, HttpServletRequest req){
         return boardService.updatePost(id, requestDto, req);
     }
 
-    @DeleteMapping("/board/{id}") //게시글 삭제
+    @DeleteMapping("/post/{id}") //게시글 삭제
     public DeleteDto deletePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto, HttpServletRequest req){
         return boardService.deletePost(id, requestDto, req);
     }
