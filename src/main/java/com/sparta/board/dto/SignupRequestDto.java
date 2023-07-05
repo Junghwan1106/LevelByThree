d3b8ca3f0bfff2dcf2e667bfb00cd88bd836ceeb
yunji <dbswl4342@gmail.com>
2023-07-05 21:51:24 +0900

    정규표현식 수정

--- src/main/java/com/sparta/board/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/board/dto/SignupRequestDto.java
@@ -9,10 +9,12 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
     @NotBlank
-    @Pattern(regexp = "[a-z0-9]{4,10}", message = "이름은 4자 이상, 10자 이하로 알파벳 소문자, 숫자로 설정해주세요.")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$",
+            message = "이름은 4자 이상, 10자 이하로 알파벳 소문자, 숫자로 설정해주세요.")
     private String username;
     @NotBlank
-    @Pattern(regexp = "[A-Za-z0-9]{8,15}", message = "비밀번호는 8자 이상, 15자 이하로 알파벳 대소문자, 숫자로 설정해주세요.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z0-9]{8,15}$",
+            message = "비밀번호는 8자 이상, 15자 이하로 알파벳 대소문자, 숫자로 설정해주세요.")
     private String password;
 
     private boolean admin = false;
